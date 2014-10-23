c93930e698c1f0dfc89fc42a3ce3954ff64d4a00
Mikela Clemmons <glassresistor@gmail.com>
2014-10-23 09:22:46 -0700

    used the cookiecutter and merged it with tweditor

---
+++ src/main.js
@@ -0,0 +1,4 @@
+//This File is browserified and put in build/tweditor.js
+//use it with the index.html as the projects main or in the case of libs as a
+//way for non commonjs projects to use the code. "packaging"
+var tweditor = require('./index');
